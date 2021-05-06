357f2892037c58cf007bf75c16f0e2644c28ec30
Simon Labonne <simon.labonne@usherbrooke.ca>
2021-05-06 18:39:02 -0400

    Update tabs.js

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,4 +1,6 @@
-$('#nav-tab a').on('click', function (event) {
-  event.preventDefault()
-  $(this).tab('show')
-})
+$(document).ready(function(){
+  $('#nav-tab a').on('click', function (event) {
+    event.preventDefault()
+    $(this).tab('show')
+  })
+});
